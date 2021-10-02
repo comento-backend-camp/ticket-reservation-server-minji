9f545b79e34b311d3e2523d681a8d38877f7ec5b
minji128 <minji.lee128@gmail.com>
2021-10-02 13:24:55 +0900

    [feature #5] Fix: 피드백 반영

--- src/main/java/com/ticket/ticketreservation/controller/MemberController.java
+++ src/main/java/com/ticket/ticketreservation/controller/MemberController.java
@@ -3,8 +3,6 @@ package com.ticket.ticketreservation.controller;
 import com.ticket.ticketreservation.domain.Member;
 import com.ticket.ticketreservation.exception.StatusCode;
 import com.ticket.ticketreservation.exception.SuccessResponse;
-import com.ticket.ticketreservation.exception.customException.UnauthorizedException;
-import com.ticket.ticketreservation.repository.MemberRepository;
 import com.ticket.ticketreservation.service.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,29 +19,22 @@ import java.util.Optional;
 public class MemberController {
 
     private final MemberService memberService;
-    private final MemberRepository memberRepository;
-
-    public MemberController(MemberService memberService, MemberRepository memberRepository) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
-        this.memberRepository = memberRepository;
     }
 
     /* 회원가입(이메일 등록) */
     @PostMapping("/join")
     public ResponseEntity join(@RequestBody @Valid String memberEmail){
         memberService.save(memberEmail);
-        Optional<Member> member = memberRepository.findByMemberEmail(memberEmail);
+        Optional<Member> member = memberService.findByMemberEmail(memberEmail);
         return ResponseEntity.created(URI.create("/members/" + memberEmail)).body(new SuccessResponse(StatusCode.CREATED, member));
     }
 
     /* 이메일 인증 */
     @GetMapping("/members/{memberEmail}")
     public ResponseEntity verifyEmail(@PathVariable(value = "memberEmail") @Email String memberEmail){
-        Optional<Member> member = memberRepository.findByMemberEmail(memberEmail);
-        if (member.isPresent()) {
-            return ResponseEntity.ok().body(new SuccessResponse(StatusCode.OK, member));
-        } else{
-            throw new UnauthorizedException();
-        }
+        Optional<Member> member = memberService.findByMemberEmail(memberEmail);
+        return ResponseEntity.ok().body(new SuccessResponse(StatusCode.OK, member));
     }
 }
