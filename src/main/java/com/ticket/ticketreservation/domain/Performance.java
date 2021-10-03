41cef1856ae25d83decdc5afd72a8b035ff8ad07
minji128 <minji.lee128@gmail.com>
2021-10-04 06:43:14 +0900

    [feature #9] Add: Performance 도메인에 @Builder 추가

--- src/main/java/com/ticket/ticketreservation/domain/Performance.java
+++ src/main/java/com/ticket/ticketreservation/domain/Performance.java
@@ -1,6 +1,7 @@
 package com.ticket.ticketreservation.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table
 public class Performance {
     @Id
