015b4541333f3c93e73c5d03c347393622ffe630
minji128 <minji.lee128@gmail.com>
2021-10-03 13:40:08 +0900

    [feature #9] Add: 특정 날짜의 특정 공연 정보 조회 기능

--- src/main/java/com/ticket/ticketreservation/service/PerformanceService.java
+++ src/main/java/com/ticket/ticketreservation/service/PerformanceService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PerformanceService {
@@ -16,13 +17,18 @@ public class PerformanceService {
         this.performanceRepository = performanceRepository;
     }
 
-    /* 현재 날짜에 예약가능한 특정 공연 정보 조회(날짜 오름차순) */
-    public List<Performance> showPerformanceInfo(String title){
-        LocalDate today = LocalDate.now();
-        List<Performance> performanceList = performanceRepository.findPerformanceList(title, today, today);
+    /* 특정 날짜에서 예약가능한 특정 공연들 정보 조회(날짜 오름차순) */
+    public List<Performance> showPerformanceList(String title, LocalDate date){
+        List<Performance> performanceList = performanceRepository.findPerformanceList(title, date, date);
         if (performanceList.isEmpty()) {
             throw new ResourceNotFoundException("공연 조회 결과 없음");
         }
-        return performanceRepository.findPerformanceList(title, today, today);
+        return performanceList;
+    }
+
+    /* 특정 날짜의 특정 공연 정보 조회 */
+    public Performance showPerformanceInfo(String title, LocalDate startDate, LocalDate endDate){
+        Optional<Performance> performance = performanceRepository.findByTitleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(title, startDate, endDate);
+        return performance.orElseThrow(() -> new ResourceNotFoundException("공연 조회 결과 없음"));
     }
 }
