ee1f5ddb7dd0db38fe284cea0accb8a06d1700a2
minji128 <minji.lee128@gmail.com>
2021-10-04 08:04:35 +0900

    [feature #12] Add: 공연 예약 정보 조회 기능

--- src/main/java/com/ticket/ticketreservation/service/BookingService.java
+++ src/main/java/com/ticket/ticketreservation/service/BookingService.java
@@ -2,8 +2,11 @@ package com.ticket.ticketreservation.service;
 
 import com.ticket.ticketreservation.domain.Booking;
 import com.ticket.ticketreservation.dto.BookedSeatResponseDto;
+import com.ticket.ticketreservation.dto.BookingResponseDto;
+import com.ticket.ticketreservation.dto.MemberDto;
 import com.ticket.ticketreservation.dto.PerformanceDto;
 import com.ticket.ticketreservation.enums.SeatType;
+import com.ticket.ticketreservation.exception.customException.ResourceNotFoundException;
 import com.ticket.ticketreservation.repository.BookingRepository;
 import org.springframework.stereotype.Service;
 
@@ -38,5 +41,20 @@ public class BookingService {
             bookingList.add(BookedSeatResponseDto.from(type, seatNumberList));
         }
         return bookingList;
+    }
+
+    /* 공연 예약 정보 조회 */
+    public List<BookingResponseDto> showBookingInfo(String memberEmail){
+        MemberDto memberDto = memberService.findByMemberEmail(memberEmail);
+        List<Booking> bookingList = bookingRepository.findByMember(memberDto.toEntity());
+        if(bookingList.isEmpty()){
+            throw new ResourceNotFoundException("공연 예약 정보 없음");
+        }
+
+        List<BookingResponseDto> bookingDtoList = new ArrayList<>();
+        for(Booking booking : bookingList){
+            bookingDtoList.add(BookingResponseDto.from(booking));
+        }
+        return bookingDtoList;
     }
 }
