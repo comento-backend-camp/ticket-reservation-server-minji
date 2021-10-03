4d0b4e9498b93adca059cf6aed633b6fbc4a1eec
minji128 <minji.lee128@gmail.com>
2021-10-04 08:08:21 +0900

    [feature #12] Add: @Builder

--- src/main/java/com/ticket/ticketreservation/domain/Member.java
+++ src/main/java/com/ticket/ticketreservation/domain/Member.java
@@ -1,6 +1,7 @@
 package com.ticket.ticketreservation.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table
 public class Member {
     @Id
